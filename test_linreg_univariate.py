edbf6d4d5ab62e80d339255872dce0db2527eb9f
ntoa25001 <103017091+ntoa25001@users.noreply.github.com>
2023-06-08 23:35:06 +0700

    Update test_linreg_univariate.py

--- test_linreg_univariate.py
+++ test_linreg_univariate.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
     '''
     
     # load the data
-    filePath = "data/univariateData.dat"
+    filePath = "/content/drive/MyDrive/Homework 1/CIS419--Decision-Tree-Learning-Linear-Regression/data/univariateData.dat"
     file = open(filePath,'r')
     allData = np.loadtxt(file, delimiter=',')
 
